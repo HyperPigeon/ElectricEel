3859de0498e5ea084237699fc382404a01bf4e88
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2023-08-22 21:11:17 -0400

    Adjusted yaw and pitch to make eel not look super weird when moving

--- src/main/java/net/hyper_pigeon/electriceel/entity/ElectricEelEntity.java
+++ src/main/java/net/hyper_pigeon/electriceel/entity/ElectricEelEntity.java
@@ -70,7 +70,7 @@ public class ElectricEelEntity extends WaterCreatureEntity implements MultipartE
     public ElectricEelEntity(EntityType<? extends WaterCreatureEntity> entityType, World world) {
         super(entityType, world);
         this.navigation = new AmphibiousNavigation(this, world);
-        this.moveControl = new AquaticMoveControl(this, 65, 10, 0.15f, 0.25f, true);
+        this.moveControl = new AquaticMoveControl(this, 85, 10, 0.15f, 0.25f, true);
         this.lookControl = new AquaticLookControl(this,20);
 
         for(int i = 0; i < bodySegments.length; i++){
